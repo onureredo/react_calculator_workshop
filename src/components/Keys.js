22f07f92dc89b2c643bd15109bca56f99b93b245
Onur Eredo <onureredo@yahoo.com>
2023-01-09 09:55:21 +0100

    keys comp

---
+++ src/components/Keys.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Keys = () => {
+  return (
+    <div className="keys">
+      <span>7</span>
+      <span>8</span>
+      <span>9</span>
+      <span className="operator">+</span>
+      <span>4</span>
+      <span>5</span>
+      <span>6</span>
+      <span className="operator">-</span>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+      <span className="operator">/</span>
+      <span>0</span>
+      <span>.</span>
+      <span className="eval">=</span>
+      <span className="operator">x</span>
+    </div>
+  );
+};
+
+export default Keys;
