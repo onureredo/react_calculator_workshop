835434cced977e410d064b36c5c22ce835f439e4
Onur Eredo <onureredo@yahoo.com>
2023-01-09 09:55:50 +0100

    screen comp

---
+++ src/components/Screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Screen = () => {
+  return (
+    <div className="top">
+      <span className="clear">C</span>
+      <div className="screen"></div>
+    </div>
+  );
+};
+
+export default Screen;
